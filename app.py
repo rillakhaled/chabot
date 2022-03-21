6751ba722e75c7d7355dbddf75141bf32fc575c6
Rilla Khaled <rilla.khaled@gmail.com>
2022-03-21 11:04:50 -0400

    reintroducing some older code

--- app.py
+++ app.py
@@ -9,30 +9,33 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'helloeiwur92843uqghvnefjancwqefjfjewngvve'
 messages = []
 
-@app.route('/', methods=['POST'])
+
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    incoming_msg = request.form['content']
+    if request.method == 'POST':
+        incoming_msg = request.form['content']
 
-    if not incoming_msg:
-        flash('Content is required!')
-    else:
+        if not incoming_msg:
+            flash('Content is required!')
+        else:
 
-        # obtain chat_log
-        chat_log = session.get('chat_log')
 
-        # add our new message to the message list
-        messages.insert(0, {'content': incoming_msg})
+            # obtain chat_log
+            chat_log = session.get('chat_log')
 
-        # call ask to get a response
-        bot_answer = ask(incoming_msg, chat_log)
-        messages.insert(0, {'content': bot_answer})
+            # add our new message to the message list
+            messages.insert(0, {'content': incoming_msg})
 
-        # update our session's chat_log
-        session['chat_log'] = update_log(bot_answer, chat_log)
+            # call ask to get a response
+            bot_answer = ask(incoming_msg, chat_log)
+            messages.insert(0, {'content': bot_answer})
 
-        # refresh the page with our updated messages
-        return redirect(url_for('index'))
+            # update our session's chat_log
+            session['chat_log'] = update_log(bot_answer, chat_log)
 
+            # refresh the page with our updated messages
+            return redirect(url_for('index'))
+            
     return render_template('index.html', messages=messages)
 
 if __name__ == "__main__":
