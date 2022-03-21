89cd90f84f5ba46b5be2c256e5466f7560d5438d
Rilla Khaled <rilla.khaled@gmail.com>
2022-03-21 14:03:14 -0400

    debugging

--- app.py
+++ app.py
@@ -10,28 +10,27 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
 messages = []
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/', methods=('POST'))
 def index():
     # obtain chat_log
     chat_log = session.get('chat_log')
 
-    if request.method == 'POST':
-        incoming_msg = request.form['content']
-        if not incoming_msg:
-            flash('Content is required!')
-        else:
-            # add our new message to the message list
-            messages.insert(0, {'content': incoming_msg})
+    incoming_msg = request.form['content']
+    if not incoming_msg:
+        flash('Content is required!')
+    else:
+        # add our new message to the message list
+        messages.insert(0, {'content': incoming_msg})
 
-            # obtain a response, update our session's chat_log
-            bot_answer = ask(incoming_msg, chat_log)
-            session['chat_log'] = update_log(incoming_msg, bot_answer, chat_log)
+        # obtain a response, update our session's chat_log
+        bot_answer = ask(incoming_msg, chat_log)
+        session['chat_log'] = update_log(incoming_msg, bot_answer, chat_log)
 
-            # add our new message to the message list
-            messages.insert(0, {'content': bot_answer})
+        # add our new message to the message list
+        messages.insert(0, {'content': bot_answer})
 
-            # refresh the page with our updated messages
-            return redirect(url_for('index'))
+        # refresh the page with our updated messages
+        return redirect(url_for('index'))
 
     return render_template('index.html', messages=messages)
 
