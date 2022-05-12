78314e8e4594236a545be7b9419ef417b5830f3d
Rilla Khaled <rilla.khaled@gmail.com>
2022-05-12 13:59:54 -0400

    seeing whether session => local  fixes the bug

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, session
 from gptqueries import ask, update_log
 import sys
-import logging
 import secrets
-
-logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
@@ -25,7 +22,7 @@ def index():
 
             # obtain a response, update our session's chat_log
             bot_answer = ask(incoming_msg, chat_log)
-            session['chat_log'] = update_log(incoming_msg, bot_answer, chat_log)
+            local['chat_log'] = update_log(incoming_msg, bot_answer, chat_log)
 
             # add our new message to the message list
             messages.insert(0, {'content': bot_answer})
